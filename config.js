5291620f665c8dff23836b3c14aee077429150b7
Whoiscasino <sarfascasino@gmail.com>
2024-11-16 21:33:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "anoyXLQR#qZ9d_4G-VxFHbNEDkrVmY_lR2we6w7-t_nZCYdhhYQI",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
